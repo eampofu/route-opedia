a011a0648c81845d9170f887a54ce94721d176e1
Eddington <eampofu@gmail.com>
2024-02-26 22:17:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/images/react.svg'
+
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header></Header>
+    </>
+     
+  )
+}
+
+export default App
