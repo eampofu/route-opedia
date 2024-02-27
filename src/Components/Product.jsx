97ad585409703ebc32a3bcd57fbc8227c18dbd26
Eddington <eampofu@gmail.com>
2024-02-27 16:30:05 +0200

    implemented use of the Navigate hook instead of the useNavigate hook

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate,Navigate  } from "react-router-dom";
 
 export default function Product() {
-  return (
-    <div>Product</div>
-  )
+	const navigate = useNavigate();
+	const [gotoProduct, setGotToProduct] = useState(() => {
+		return false;
+	});
+	return (
+		<div>
+			Product
+			<button
+				className="btn btn-primary"
+				onClick={() => {
+					navigate("/product/create");
+				}}
+			>
+				Add Product{" "}
+			</button>
+			<Link to={"/product/details/5"}>
+				{" "}
+				<button>Navigate to product Detail -5</button>
+			</Link>
+            {/* using the Navigate component instead of the useNavigate Hook */}
+			{gotoProduct && <Navigate to="/product/details/3" />}
+			<button
+				onClick={() => {
+					setGotToProduct({ gotoProduct: true });
+				}}
+			>
+				Navigate to product-3(UseState)
+			</button>
+		</div>
+	);
 }
